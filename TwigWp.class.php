6d616327ebaeffae022a567c31ac81e2acef3198
yanickouellet <yanickouellet555@gmail.com>
2013-07-05 11:26:08 -0400

    Initial commit

---
+++ TwigWp.class.php
@@ -0,0 +1,63 @@
+<?php
+
+if ( !class_exists('TwigWp') ) {
+
+	/**
+	 * Class ZRoles
+	 * Main class of the plugin, using singleton pattern
+	 */
+	class TwigWp {
+
+		/**
+		 * @var Twig_Environment
+		 */
+		protected $twig;
+
+		/**
+		 * Initialise the plugin with action and hooks
+		 */
+		private function __construct()
+		{
+			require_once 'twig/lib/Twig/Autoloader.php';
+			Twig_Autoloader::register();
+
+			$loader = new Twig_Loader_Filesystem('/');
+			$this->twig = new Twig_Environment($loader, array(
+				'cache' => plugin_dir_path(__FILE__) . '/cache',
+			));
+		}
+
+		/**
+		 * Render a twig template
+		 *
+		 * @param $template
+		 * @param $params
+		 *
+		 * @return string
+		 */
+		public function render($template, $params = array())
+		{
+			return $this->twig->render($template, $params);
+		}
+
+		public static function renderStatic($template, $params = array())
+		{
+			$core = self::get_instance();
+			return $core->render($template, $params);
+		}
+
+
+		/**
+		 * Get an instance of ZDebug
+		 */
+		public static function get_instance()
+		{
+			static $core;
+			if ( !$core ) {
+				$core = new self();
+			}
+
+			return $core;
+		}
+	}
+}
---
+++ twig-wp.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Twig templates
+Plugin URI: http://productionszani.com
+Description: A plugin to add roles
+Version: 0.1
+Author: Yanick Ouellet - Productions Zani
+Author URI: http://productionszani.com
+License: GPL2
+*/
+
+require 'TwigWp.class.php';
+
+//$core = TwigWP::get_instance();
